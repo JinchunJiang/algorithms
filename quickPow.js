a6c367e26b498ab92f6e0278753b17886fc94b25
jiangjc <592791588@qq.com>
2022-05-18 22:16:50 +0800

    feat:add quick pow

---
+++ quickPow.js
@@ -0,0 +1,13 @@
+// 快速幂算法
+function quickPow(a, b) {
+  let ans = 1;
+  // 将b用二进制表示进行次方运算
+  while (b) {
+    if (b & (1 !== 0)) ans *= a;
+    a *= a;
+    b >>= 1;
+  }
+  return ans;
+}
+
+console.log(quickPow(5, 4));
